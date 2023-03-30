8c89e2a7ec6d2803a4e60a786a2d6ee8d78b8e4d
Wang Jia-Rhon <lejacp@gmail.com>
2023-03-30 22:26:47 +0800

    Input System Refactor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,27 +6,11 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private GameInput gameInput;
     private bool isWalking;
     private void Update()
     {
-        Vector2 inputVector = Vector2.zero;
-        if(Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         isWalking = moveDir != Vector3.zero;
